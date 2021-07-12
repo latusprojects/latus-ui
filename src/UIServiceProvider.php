30288ab717e8fc51d9c8e25ba8e5c22a85df7b71
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-12 02:00:19 +0200

    Create composer.json and UIServiceProvider.php

---
+++ src/UIServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Latus\UI;
+
+use Illuminate\Support\ServiceProvider;
+
+class UIServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
