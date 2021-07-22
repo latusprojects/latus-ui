f3fa92064694dd1445d02b6bd005fca95f3825f5
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-22 14:50:31 +0200

    Add binding of PageSettingRepository.php contract to eloquent implementation, and loading of migrations to UIServiceProvider.php

--- src/Providers/UIServiceProvider.php
+++ src/Providers/UIServiceProvider.php
@@ -4,6 +4,8 @@ namespace Latus\UI\Providers;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
+use Latus\UI\Repositories\Contracts\PageSettingRepository as PageSettingRepositoryContract;
+use Latus\UI\Repositories\Eloquent\PageSettingRepository;
 
 class UIServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,10 @@ class UIServiceProvider extends ServiceProvider
      */
     public function register()
     {
+
+        if (!$this->app->bound(PageSettingRepositoryContract::class)) {
+            $this->app->bind(PageSettingRepositoryContract::class, PageSettingRepository::class);
+        }
 
         $this->app->singleton('widgets', function () {
             return new Collection();
@@ -31,5 +37,6 @@ class UIServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 }
