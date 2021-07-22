7c059e186592c5cb0ec996fdac85f13d44b29633
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-22 16:13:08 +0200

    Add method for retrieving page-settings, add getter and setter for content, add methods from contract

--- src/Components/Contracts/PageComponent.php
+++ src/Components/Contracts/PageComponent.php
@@ -4,10 +4,18 @@
 namespace Latus\UI\Components\Contracts;
 
 
+use Illuminate\Support\Collection;
+
 interface PageComponent extends Component
 {
 
     public function module(): ModuleComponent;
 
     public function compose();
+
+    public function getSettings(): Collection;
+
+    public function getContent(): mixed;
+
+    public function setContent(mixed $content);
 }
--- src/Components/PageComponent.php
+++ src/Components/PageComponent.php
@@ -3,19 +3,45 @@
 
 namespace Latus\UI\Components;
 
+use Illuminate\Support\Collection;
 use Latus\UI\Components\Contracts\ModuleComponent;
 use Latus\UI\Components\Contracts\PageComponent as PageComponentContract;
+use Latus\UI\Services\PageSettingService;
 
 abstract class PageComponent extends Component implements PageComponentContract
 {
     public function __construct(
-        protected ModuleComponent &$moduleComponent
+        protected ModuleComponent &$moduleComponent,
+        protected mixed $content = null
     )
+    {
+    }
+
+    public function getSettings(): Collection
+    {
+        return (app(PageSettingService::class))->getSettings($this->module()->getName(), $this->getName());
+    }
+
+    public function register()
+    {
+    }
+
+    public function compose()
     {
     }
 
     public function module(): ModuleComponent
     {
         return $this->moduleComponent;
+    }
+
+    public function getContent(): mixed
+    {
+        return $this->content;
+    }
+
+    public function setContent(mixed $content)
+    {
+        $this->content = $content;
     }
 }
