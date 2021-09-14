869de7ffe40aa516fb379c3175c1121e11ab9200
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-14 19:52:20 +0200

    Fix dependency might not be bound

--- src/Providers/Traits/ProvidesWidgets.php
+++ src/Providers/Traits/ProvidesWidgets.php
@@ -4,14 +4,18 @@
 namespace Latus\UI\Providers\Traits;
 
 
+use Latus\Laravel\Http\Middleware\BuildPackageDependencies;
+
 trait ProvidesWidgets
 {
     use RequiresComponentService;
 
     protected function provideWidgets(array $widgetClasses)
     {
-        foreach ($widgetClasses as $widgetName => $widgetClass) {
-            $this->getComponentService()->provideWidget($widgetClass, $widgetName);
-        }
+        BuildPackageDependencies::addDependencyClosure(function () use ($widgetClasses) {
+            foreach ($widgetClasses as $widgetName => $widgetClass) {
+                $this->getComponentService()->provideWidget($widgetClass, $widgetName);
+            }
+        });
     }
 }
