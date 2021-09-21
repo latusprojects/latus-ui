78d6eed6f005e3e40168cfde5361d339c9c01e65
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-21 16:39:47 +0200

    Update NavigationWidget.php contract and abstract to match new builder-architecture

--- src/Widgets/Contracts/NavigationWidget.php
+++ src/Widgets/Contracts/NavigationWidget.php
@@ -8,19 +8,6 @@ use Illuminate\Support\Collection;
 
 interface NavigationWidget
 {
-
-    public function &getItems(): Collection;
-
-    public function validateItems();
-
-    public function levels(): int;
-
-    public function supportsPermissions(): bool;
-
-    public function putBefore(string $indexKey, array $items);
-
-    public function putAfter(string $indexKey, array $items);
-
     public function setReference(string $reference);
 
     public function getReference(): string|null;
